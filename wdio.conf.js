09f7ab4c065ccd3546fe768dd19147faf702d926
Ayoub JEMMAL <ayoubjemmal1@gmail.com>
2024-02-19 17:45:14 -0600

    baseUrl UPDATED

--- wdio.conf.js
+++ wdio.conf.js
@@ -28,7 +28,7 @@ exports.config = {
 ],
     logLevel: 'error',
     bail: 0,
-    baseUrl: 'https://cnt-67711b6a-1794-40b5-acb3-9ddd2e002428.containerhub.tripleten-services.com/',
+    baseUrl: 'https://cnt-2dff976c-5dc7-45c7-9d5e-67f194a20d22.containerhub.tripleten-services.com/',
     waitforTimeout: 10000,
     connectionRetryTimeout: 120000,
     connectionRetryCount: 3,
